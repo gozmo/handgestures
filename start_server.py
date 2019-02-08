95102b2c4badc1ea7208067082f8f9d983e48094
pontus <you@example.com>
2019-02-08 20:29:37 +0100

    template wip

--- start_server.py
+++ start_server.py
@@ -1,64 +1,16 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, template_folder="handsignals/server/templates")
 #!/bin/python
-
-import os
-from flask import Flask, Response, request, abort, render_template_string, send_from_directory
-from PIL import Image
+from flask import Flask, Response, request, abort, render_template_string, send_from_directory,render_template
 from io import StringIO
 
 from handsignals.dataset import file_utils
+from handsignals import main
+from handsignals.server.templates.data import data_template
 
 
-WIDTH = 640 
-HEIGHT = 400
-
-LABELS = ["none", "metal", "ok", "victory"]
 
 
-TEMPLATE = '''
-<!DOCTYPE html>
-<html>
-<head>
-    <title></title>
-    <meta charset="utf-8" />
-    <style>
-body {
-    margin: 0;
-    background-color: #333;
-}
-.image {
-    display: block;
-    margin: 2em auto;
-    background-color: #444;
-    box-shadow: 0 0 10px rgba(0,0,0,0.3);
-}
-img {
-    display: block;
-}
-    </style>
-</head>
-<body>
-    {% for image in images %}
-        <a class="image" href="{{image.src}}" style="width: {{ image.width }}px; height: {{ image.height }}px">
-            <img src={{image.src}} width="{{ image.width }}" height="{{ image.height }}" />
-        </a>
-
-	<form action="" method="POST" onsubmit="" name={{image.src}}>
-	{% for label in labels %}
-            <p>
-	    {% if label == "none" %}
-		<input type="radio" name={{image.src}} id={{label}} value={{label}} checked> {{label}} 
-	    {% else %}
-		<input type="radio" name={{image.src}} id={{label}} value={{label}}>         {{label}}
-	    {% endif %}
-            </p>
-	{% endfor %}
-        <p><input type=submit value=Next></p>
-	</form>
-    {% endfor %}
-</body>
-'''
 
 @app.route('/<path:filename>')
 def image(filename):
@@ -80,41 +32,32 @@ def image(filename):
 
     return send_from_directory('.', filename)
 
-@app.route('/annotate', methods=["GET", "POST"])
-def annotate():
-    if request.method == "POST":
-        items = request.form.to_dict().items()
-        (image_path, label) = list(items)[0]
-        file_utils.move_image_to_label(image_path, label)
-
-    images = []
-    for root, dirs, files in os.walk('dataset/unlabeled'):
-        for filename in [os.path.join(root, name) for name in files]:
-            if not filename.endswith('.jpg'):
-                continue
-            im = Image.open(filename)
-            w, h = im.size
-            aspect = 1.0*w/h
-            if aspect > 1.0*WIDTH/HEIGHT:
-                width = min(w, WIDTH)
-                height = width/aspect
-            else:
-                height = min(h, HEIGHT)
-                width = height*aspect
-            images.append({
-                'width': int(width),
-                'height': int(height),
-                'src': filename
-            })
-
-    return render_template_string(TEMPLATE,
-                                  images=images,
-                                  labels=LABELS)
-    
 @app.route('/record', methods=["GET", "POST"])
 def record():
     if request.method == "POST":
-        #setup a record html
-        pass
+        post_dict= request.form.to_dict()
+        frames_to_capture= post_dict["frames_to_capture"]
+        main.record(frames_to_capture)
+
+    return render_template("record/record.html")
+
+@app.route("/data")
+@app.route("/data/<task>", methods=["GET", "POST"])
+def data(task=None):
+    print(task)
+    if task is None:
+        return render_template(f"data/base.html")
+    elif task == "annotate":
+        return data_template.render_annotate(request)
+    else:
+        return render_template(f"data/{task}.html")
+
+@app.route("/index")
+def index():
+    return render_template("base.html")
+
+@app.route("/status")
+def status():
+    return "status: ok"
 
 app.run(debug=True, host='::')
